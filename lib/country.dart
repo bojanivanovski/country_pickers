6b2a8bda2f3752b44ceca03d4423e23b02dbf370
boboskmk <bivanovski@dynamichealthit.com>
2020-03-22 13:35:50 +0100

    added international licence plate country code, editted some of the countries out

--- lib/country.dart
+++ lib/country.dart
@@ -3,12 +3,15 @@ class Country {
   final String isoCode;
   final String iso3Code;
   final String phoneCode;
-  Country({this.isoCode, this.iso3Code, this.phoneCode, this.name});
+  final String ilpcCode; //international licence plate country code;
+  Country(
+      {this.isoCode, this.iso3Code, this.phoneCode, this.name, this.ilpcCode});
 
   factory Country.fromMap(Map<String, String> map) => Country(
         name: map['name'],
         isoCode: map['isoCode'],
         iso3Code: map['iso3Code'],
         phoneCode: map['phoneCode'],
+        ilpcCode: map['ilpcCode'] ?? "",
       );
 }
